c48397b598d7aa536e6504501a7fb6a989b87c96
Kerem Yardan <keremyrdn@hotmail.com>
2024-06-10 01:52:35 +0300

    Model and mapper classes created for chat entity

---
+++ backend/src/main/java/com/instagram/backend/mapper/ChatMapper.java
@@ -0,0 +1,23 @@
+package com.instagram.backend.mapper;
+
+import com.instagram.backend.model.Chat;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+public interface ChatMapper {
+    @Insert("INSERT INTO chats(session_id, user_id, chat_content, chat_timestamp)" +
+            " values(#{sessionId}, #{userId}, #{chatContent},#{chatTimestamp})")
+    int createChat(String sessionId, Integer userId,String chatContent, String chatTimestamp);
+
+    @Select("SELECT * FROM chats WHERE session_id = #{sessionId} ORDER BY chat_timestamp DESC")
+    List<Chat> getChatsBySessionId(String sessionId);
+
+    @Select("SELECT chat_timestamp,chat_content FROM chats WHERE session_id = #{sessionId} ORDER BY chat_timestamp DESC LIMIT 1")
+    Chat getLatestTime(String sessionId);
+}
---
+++ backend/src/main/java/com/instagram/backend/model/Chat.java
@@ -0,0 +1,9 @@
+package com.instagram.backend.model;
+
+public class Chat {
+    Integer chatId;
+    String sessionId;
+    Integer userId;
+    String chatContent;
+    String chatTimestamp;
+}
