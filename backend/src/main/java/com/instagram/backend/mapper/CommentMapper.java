7c4c7de142696853b3b6a8704202bed6a6b796dd
Kerem Yardan <keremyrdn@hotmail.com>
2024-06-16 01:36:21 +0300

    mapper (dao) class multiplied

--- backend/src/main/java/com/instagram/backend/mapper/CommentMapper.java
+++ backend/src/main/java/com/instagram/backend/mapper/CommentMapper.java
@@ -2,10 +2,14 @@ package com.instagram.backend.mapper;
 
 import com.instagram.backend.model.Comment;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Mapper
+@Component
 public interface CommentMapper {
 
     @Insert("INSERT INTO comments(post_id, commented_id,comment_content,comment_timestamp, commenter_name, commenter_avatar) " +
---
+++ backend/src/main/java/com/instagram/backend/mapper/FollowMapper.java
@@ -0,0 +1,11 @@
+package com.instagram.backend.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component
+public interface FollowMapper {
+
+}
