8137647d19bc994f7381c1de3d8fb462a15c40ff
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-04 00:18:46 +0300

    PostMapper dao class created

---
+++ backend/src/main/java/com/instagram/backend/mapper/PostMapper.java
@@ -0,0 +1,24 @@
+package com.instagram.backend.mapper;
+
+import com.instagram.backend.model.Post;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Mapper
+@Component
+public interface PostMapper {
+    @Insert("INSERT INTO posts(post_identifier, image_url, user_id, post_date, post_location," +
+        "post_caption, post_alt, post_comments, post_likes, allow_comment, allow_like)" +
+            "values(#{postIdentifier}, #{imageUrl}, #{userId}, #{postDate}, #{postLocation}, #{postCaption}," +
+            "#{postAlt}, #{postComments}, #{postLikes}, #{allowComment}, #{allowLike})")
+    int insertPost (String postIdentifier, String imageUrl, int userId, String postDate, String postLocation, String postCaption, String postAlt,
+                    int postComments, int postLikes, boolean allowComment, boolean allowLike);
+
+    @Select("SELECT * FROM post WHERE user_id = #{id}")
+    List<Post> getPostById(int id);
+
+
+}
