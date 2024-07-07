8a47f304ec6275320983401917a84c46654309eb
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-08 01:17:08 +0300

    SaveMapper and SessionMapper interfaces are created and queries have been prepared

---
+++ backend/src/main/java/com/instagram/backend/mapper/SaveMapper.java
@@ -0,0 +1,23 @@
+package com.instagram.backend.mapper;
+
+import com.instagram.backend.model.Save;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+public interface SaveMapper {
+    @Insert("INSERT INTO saves(user_id, post_id, save_timestamp) values(#{userId},#{postId}, #{saveTimestamp})")
+    int insertSave(Integer userId, Integer postId, String saveTimestamp);
+
+    @Delete("DELETE FROM saves WHERE user_id = #{userId} and post_id = #{postId}")
+    int deleteSave (Integer userId, Integer postId);
+
+    @Select("SELECT * FROM saves WHERE user_id = #{userId} and post_id= #{postId}")
+    List<Save> checkIsSaving(Integer userId, Integer postId);
+}
---
+++ backend/src/main/java/com/instagram/backend/mapper/SessionMapper.java
@@ -0,0 +1,23 @@
+package com.instagram.backend.mapper;
+
+import com.instagram.backend.model.DetailedSession;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+public interface SessionMapper {
+    @Insert("INSERT INTO sessions (session_id, userA_id, userA_name, userA_avatar, userB_id, userB_avatar, session_timestamp)" +
+    "values (#{sessionId}, #{userAId}, #{userAName}, #{userAAvatar}, #{userBId}, #{userBName}, #{userBAvatar}, #{sessionTimestamp})")
+    int createSession (String sessionId, Integer userAId, String UserAName, String userAAvatar, Integer userBId, String userBName, String userBAvatar, String sessionTimestamp);
+
+    @Select("SELECT * FROM sessions WGERE userA_id = #{id} OR userB_id = #{id}")
+    List<DetailedSession> fetchSessionsById (Integer id);
+
+    @Select("SELECT * FROM sessions WHERE session_id = #{sessionId}")
+    List<DetailedSession> getSessionBySessionId (String sessionId);
+}
