d6e84fdf0c3ac9b92ca8a94d9e8dc81bdc6a152a
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-11 23:36:07 +0300

    Account controller class expanded

--- backend/src/main/java/com/instagram/backend/controller/AccountController.java
+++ backend/src/main/java/com/instagram/backend/controller/AccountController.java
@@ -1,11 +1,17 @@
 package com.instagram.backend.controller;
 
 import com.instagram.backend.mapper.UserMapper;
+import com.instagram.backend.model.Image;
+import com.instagram.backend.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -14,4 +20,16 @@ public class AccountController {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    public User user;
+
+    @Autowired
+    public Image image;
+
+    @GetMapping("accounts/validate/email/{email}")
+    public boolean checkEmail(@PathVariable("email") String email) {
+        List<String> allEmails = userMapper.getAllEmails();
+        return !allEmails.contains(email);
+    }
 }
