64798de33fd303c837f4fdb54e64f879cff9cd32
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-15 00:08:31 +0300

    Signup endpoint has been created for AccountController class

--- backend/src/main/java/com/instagram/backend/controller/AccountController.java
+++ backend/src/main/java/com/instagram/backend/controller/AccountController.java
@@ -1,15 +1,17 @@
 package com.instagram.backend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.instagram.backend.mapper.UserMapper;
 import com.instagram.backend.model.Image;
 import com.instagram.backend.model.User;
+import com.instagram.backend.utils.JWTUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +39,17 @@ public class AccountController {
     public boolean checkEmail(@PathVariable("email") String email) {
         List<String> allEmails = userMapper.getAllEmails();
         return !allEmails.contains(email);
+    }
+
+    @PostMapping("/accounts/signup")
+    public String signup(@RequestBody String content) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        user = objectMapper.readValue(content, User.class);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        String token = JWTUtil.sign(user.getUserName(), user.getUserId());
+        userMapper.insertUser(user.getUserName(), user.getEmail(), user.getPassword(),user.getFullName(), user.getAvatar());
+        return token;
     }
 }
