0fdb053b1fb3a04ba7566f5ed470d18d91c8d7b3
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-18 00:56:11 +0300

    ChatController class created

---
+++ backend/src/main/java/com/instagram/backend/controller/ChatController.java
@@ -0,0 +1,13 @@
+package com.instagram.backend.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@CrossOrigin
+public class ChatController {
+    protected static final Logger logger = LoggerFactory.getLogger(AccountController.class);
+}
