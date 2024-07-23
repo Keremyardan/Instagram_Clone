53c56559a880d5e6a19e40994ff5232905bb4efe
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-23 23:06:04 +0300

    FollowController class completed

--- backend/src/main/java/com/instagram/backend/controller/FollowController.java
+++ backend/src/main/java/com/instagram/backend/controller/FollowController.java
@@ -1,5 +1,7 @@
 package com.instagram.backend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.instagram.backend.mapper.FollowMapper;
 import com.instagram.backend.mapper.UserMapper;
 import com.instagram.backend.model.*;
@@ -7,10 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,7 +39,7 @@ public class FollowController {
     }
 
     @GetMapping("/follows/recent")
-    public Object getRecentFollows(@RequestParam("userId") Integer userId, @RequestParam("limit") Integer limit){
+    public Object getRecentFollows(@RequestParam("userId") Integer userId, @RequestParam("limit") Integer limit) {
         return followMapper.getAllRecentFollows(userId, limit);
     }
 
@@ -54,7 +53,41 @@ public class FollowController {
         }
         List<MutualResult> res = new ArrayList<>();
         for (int i = 0; i < queryUsers.size(); i++) {
-        List<MutualFriend> temp = userMapper.getMutualFriends(userId, queryUsers.get(i).getUserId());
+            List<MutualFriend> temp = userMapper.getMutualFriends(userId, queryUsers.get(i).getUserId());
+            if (temp.size() != 0 && !friends.contains(queryUsers.get(i).getUserId())) {
+                MutualResult mutualResult = new MutualResult();
+                mutualResult.setUserId(queryUsers.get(i).getUserId());
+                mutualResult.setUserAvatar(queryUsers.get(i).getAvatar());
+                mutualResult.setUserName(queryUsers.get(i).getUserName());
+                mutualResult.setMutualNumber(temp.size());
+                res.add(mutualResult);
+            }
+        }
+        return res;
     }
+
+    @GetMapping("/follows/follow")
+    public Integer follow(@RequestBody String content) throws JsonProcessingException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        follow = objectMapper.readValue(content, Follow.class);
+        followMapper.inserfFollow(follow.getFollowerId(), follow.getFollowedId(), follow.getFollowTimestamp());
+        return 1;
+    }
+
+    @GetMapping("/follows/unfollow")
+    public Integer unfollow(@RequestBody String content) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        follow = objectMapper.readValue(content, Follow.class);
+        followMapper.deleteFollow(follow.getFollowerId(), follow.getFollowedId());
+        return 1;
+    }
+
+    @PostMapping("/follows/check")
+    public boolean checkIsFollowing(@RequestBody String content) throws JsonProcessingException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        follow = objectMapper.readValue(content,Follow.class);
+        logger.info(follow.toString());
+        List<Follow> res = followMapper.checkIsFollowing(follow.getFollowId(), follow.getFollowedId());
+        return res.size() == 1;
     }
 }
